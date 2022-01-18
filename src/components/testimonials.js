7af0c805df40edf8b434fe05ab7d7d1900fc0c17
Avinash Gautam <avinashjini18@gmail.com>
2022-01-19 01:27:36 +0530

    Test indentation

--- src/components/testimonials.js
+++ src/components/testimonials.js
@@ -16,9 +16,9 @@ class Testimonials extends React.Component {
                             <h3>Saul Goodman</h3>
                             <h4>Ceo &amp; Founder</h4>
                             <p>
-                            <i className="bx bxs-quote-alt-left quote-icon-left"></i>
-                            Proin iaculis purus consequat sem cure digni ssim donec porttitora entum suscipit rhoncus. Accusantium quam, ultricies eget id, aliquam eget nibh et. Maecen aliquam, risus at semper.
-                            <i className="bx bxs-quote-alt-right quote-icon-right"></i>
+                                <i className="bx bxs-quote-alt-left quote-icon-left"></i>
+                                Proin iaculis purus consequat sem cure digni ssim donec porttitora entum suscipit rhoncus. Accusantium quam, ultricies eget id, aliquam eget nibh et. Maecen aliquam, risus at semper.
+                                <i className="bx bxs-quote-alt-right quote-icon-right"></i>
                             </p>
                         </div>
 
@@ -27,9 +27,9 @@ class Testimonials extends React.Component {
                             <h3>Sara Wilsson</h3>
                             <h4>Designer</h4>
                             <p>
-                            <i className="bx bxs-quote-alt-left quote-icon-left"></i>
-                            Export tempor illum tamen malis malis eram quae irure esse labore quem cillum quid cillum eram malis quorum velit fore eram velit sunt aliqua noster fugiat irure amet legam anim culpa.
-                            <i className="bx bxs-quote-alt-right quote-icon-right"></i>
+                                <i className="bx bxs-quote-alt-left quote-icon-left"></i>
+                                Export tempor illum tamen malis malis eram quae irure esse labore quem cillum quid cillum eram malis quorum velit fore eram velit sunt aliqua noster fugiat irure amet legam anim culpa.
+                                <i className="bx bxs-quote-alt-right quote-icon-right"></i>
                             </p>
                         </div>
 
@@ -38,9 +38,9 @@ class Testimonials extends React.Component {
                             <h3>Jena Karlis</h3>
                             <h4>Store Owner</h4>
                             <p>
-                            <i className="bx bxs-quote-alt-left quote-icon-left"></i>
-                            Enim nisi quem export duis labore cillum quae magna enim sint quorum nulla quem veniam duis minim tempor labore quem eram duis noster aute amet eram fore quis sint minim.
-                            <i className="bx bxs-quote-alt-right quote-icon-right"></i>
+                                <i className="bx bxs-quote-alt-left quote-icon-left"></i>
+                                Enim nisi quem export duis labore cillum quae magna enim sint quorum nulla quem veniam duis minim tempor labore quem eram duis noster aute amet eram fore quis sint minim.
+                                <i className="bx bxs-quote-alt-right quote-icon-right"></i>
                             </p>
                         </div>
 
@@ -49,9 +49,9 @@ class Testimonials extends React.Component {
                             <h3>Matt Brandon</h3>
                             <h4>Freelancer</h4>
                             <p>
-                            <i className="bx bxs-quote-alt-left quote-icon-left"></i>
-                            Fugiat enim eram quae cillum dolore dolor amet nulla culpa multos export minim fugiat minim velit minim dolor enim duis veniam ipsum anim magna sunt elit fore quem dolore labore illum veniam.
-                            <i className="bx bxs-quote-alt-right quote-icon-right"></i>
+                                <i className="bx bxs-quote-alt-left quote-icon-left"></i>
+                                Fugiat enim eram quae cillum dolore dolor amet nulla culpa multos export minim fugiat minim velit minim dolor enim duis veniam ipsum anim magna sunt elit fore quem dolore labore illum veniam.
+                                <i className="bx bxs-quote-alt-right quote-icon-right"></i>
                             </p>
                         </div>
 
@@ -60,9 +60,9 @@ class Testimonials extends React.Component {
                             <h3>John Larson</h3>
                             <h4>Entrepreneur</h4>
                             <p>
-                            <i className="bx bxs-quote-alt-left quote-icon-left"></i>
-                            Quis quorum aliqua sint quem legam fore sunt eram irure aliqua veniam tempor noster veniam enim culpa labore duis sunt culpa nulla illum cillum fugiat legam esse veniam culpa fore nisi cillum quid.
-                            <i className="bx bxs-quote-alt-right quote-icon-right"></i>
+                                <i className="bx bxs-quote-alt-left quote-icon-left"></i>
+                                Quis quorum aliqua sint quem legam fore sunt eram irure aliqua veniam tempor noster veniam enim culpa labore duis sunt culpa nulla illum cillum fugiat legam esse veniam culpa fore nisi cillum quid.
+                                <i className="bx bxs-quote-alt-right quote-icon-right"></i>
                             </p>
                         </div>
                     </OwlCarousel >
